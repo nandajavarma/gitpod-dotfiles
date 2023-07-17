a092b8401db54846fba43fe9b80440afb21ed6d8
Nandaja Varma <nandaja.varma@gmail.com>
2023-07-17 08:31:10 +0200

    fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,16 +3,19 @@ set -Eeuo pipefail
 
 git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim
+rm -rf ~/.zshrc ~/.vimrc ~/.tmux.conf ~/.vim/bundle/Vundle.vim
+ln -s ~/.dotfiles/.zshrc ~/.zshrc || true
+ln -s ~/.dotfiles/.vimrc ~/.vimrc || true
+ln -s ~/.dotfiles/.vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim || true
 
 vim +PluginInstall +qall
 
-ln -s ~/.dotfiles/.emacs.d ~/.emacs.d
-ln -s ~/.dotfiles/.doom.d ~/.doom.d
-cd .doom.d/modules/private/spacemacs
-git submodule update --init
+ln -s ~/.dotfiles/.doom.d ~/.doom.d || true
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+
+cd .doom.d/modules/private
+rm -rf spacemacs
+git clone https://github.com/chenyanming/spacemacs_module_for_doom spacemacs/ spacemacs
 cd --
 ~/.emacs.d/bin/doom sync
 
