26a7867c070aec4fe3ea5c126da6697f6d3de43f
Nandaja Varma <nandaja.varma@gmail.com>
2023-08-19 12:16:03 +0530

    remove unnecessary files

--- setup.sh
+++ setup.sh
@@ -3,11 +3,12 @@ set -Eeuo pipefail
 
 rm -rf ~/.zshrc ~/.vimrc ~/.tmux.conf ~/.vim/bundle/Vundle.vim ~/.oh-my-zsh ~/.emacs.d
 
-ln -s ~/.dotfiles/.zshrc ~/ || true
 ln -s ~/.dotfiles/.vimrc ~/ || true
 ln -s ~/.dotfiles/.tmux.conf ~/ || true
 
-ln -s ~/.dotfiles/.oh-my-zsh ~/ || true
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+ln -s ~/.dotfiles/.zshrc ~/ || true
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
