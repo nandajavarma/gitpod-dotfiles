73deb847c7f12d4b7a3ef11a76c585b9f00d38b0
Nandaja <nandaja@Nandajas-MacBook-Pro.local>
2025-03-12 12:43:03 +0100

    remove emacs deps

--- setup.sh
+++ setup.sh
@@ -8,24 +8,15 @@ rm -rf ~/.zshrc ~/.vimrc ~/.tmux.conf ~/.vim/bundle/Vundle.vim ~/.oh-my-zsh ~/.e
 ln -s ~/.dotfiles/.vimrc ~/ || true
 ln -s ~/.dotfiles/.tmux.conf ~/ || true
 
+sudo apt update
+sudo apt install -y tmux vim
+
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 mkdir -p ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 ln -s ~/.dotfiles/.zshrc ~/ || true
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
-ln -s ~/.dotfiles/.doom.d ~/ || true
-ln -s ~/.dotfiles/.emacs.d ~/ || true
-
-mkdir -p .doom.d/modules/private
-cd .doom.d/modules/private
-rm -rf spacemacs
-
-git clone https://github.com/chenyanming/spacemacs_module_for_doom spacemacs/
 
 echo 'alias k="kubectl"' >> ~/.zshrc
 echo 'autoload -Uz compinit && compinit' >> ~/.zshrc
@@ -35,9 +26,5 @@ git config --global url."https://".insteadOf git://
 git config --global url."https://github.com/".insteadOf ssh://git@github.com/
 git config --global url."https://github.com/".insteadOf git@github.com:
 
-~/.emacs.d/bin/doom sync &
-
 git clone git@github.com:nandajavarma/notes.git  $HOME/.deft
 cd --
-
-
