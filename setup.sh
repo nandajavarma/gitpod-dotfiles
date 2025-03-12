5df68677f9fbac4c53061aee6e643d01ae81004c
Nandaja Varma <nandaja.varma@gmail.com>
2025-03-12 13:23:45 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ git config --global push.autoSetupRemote true
 
 rm -rf ~/.zshrc ~/.vimrc ~/.tmux.conf ~/.vim/bundle/Vundle.vim ~/.oh-my-zsh ~/.emacs.d
 
-ln -s ~/.dotfiles/.vimrc ~/ || true
-ln -s ~/.dotfiles/.tmux.conf ~/ || true
+ln -s ~/.dotfiles/.vimrc ~/ || echo "could not create vimrc"
+ln -s ~/.dotfiles/.tmux.conf ~/ || echo "could not create tmux.conf"
 
 sudo apt update
 sudo apt install -y tmux vim
@@ -16,15 +16,4 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 mkdir -p ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-ln -s ~/.dotfiles/.zshrc ~/ || true
-
-echo 'alias k="kubectl"' >> ~/.zshrc
-echo 'autoload -Uz compinit && compinit' >> ~/.zshrc
-
-# hack to be able to clone private repo https://github.com/gitpod-io/gitpod/issues/1191#issuecomment-855484471
-git config --global url."https://".insteadOf git://
-git config --global url."https://github.com/".insteadOf ssh://git@github.com/
-git config --global url."https://github.com/".insteadOf git@github.com:
-
-git clone git@github.com:nandajavarma/notes.git  $HOME/.deft
-cd --
+ln -s ~/.dotfiles/.zshrc ~/ || echo "could not create zshrc"
