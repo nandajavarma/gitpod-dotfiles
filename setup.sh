a94848672be5bab9e589fcea6ccdf3a726de7985
Nandaja Varma <nandaja.varma@gmail.com>
2025-03-22 09:35:19 +0000

    Fix

--- setup.sh
+++ setup.sh
@@ -5,19 +5,19 @@ git config --global push.autoSetupRemote true
 
 rm -rf ~/.zshrc ~/.vimrc ~/.tmux.conf ~/.vim/bundle/Vundle.vim ~/.oh-my-zsh ~/.emacs.d
 
-cp -r ~/.dotfiles/.vim /home/vscode/ || echo "could not create vim for remote user"
-cp -r ~/.dotfiles/.vim ~/ || echo "could not create vim for root user"
+cp -r ~/dotfiles/.vim /home/vscode/ || echo "could not create vim for remote user"
+cp -r ~/dotfiles/.vim ~/ || echo "could not create vim for root user"
 
-cp -r ~/.dotfiles/.vimrc /home/vscode/ || echo "could not create vimrc for remote user"
-cp -r ~/.dotfiles/.vimrc ~/ || echo "could not create vimrc for root user"
+cp -r ~/dotfiles/.vimrc /home/vscode/ || echo "could not create vimrc for remote user"
+cp -r ~/dotfiles/.vimrc ~/ || echo "could not create vimrc for root user"
 
-cp -r ~/.dotfiles/.tmux.conf /home/vscode/ || echo "could not create tmux.conf for remote user"
-cp -r ~/.dotfiles/.tmux.conf ~/ || echo "could not create tmux.conf for root user"
+cp -r ~/dotfiles/.tmux.conf /home/vscode/ || echo "could not create tmux.conf for remote user"
+cp -r ~/dotfiles/.tmux.conf ~/ || echo "could not create tmux.conf for root user"
 
-cp -r ~/.dotfiles/.zshrc /home/vscode/ || echo "could not create zshrc for remote user"
-cp -r ~/.dotfiles/.zshrc ~/ || echo "could not create zshrc for root user"
+cp -r ~/dotfiles/.zshrc /home/vscode/ || echo "could not create zshrc for remote user"
+cp -r ~/dotfiles/.zshrc ~/ || echo "could not create zshrc for root user"
 
-cp -r ~/.dotfiles/.cursor/rules/* /workspace/.cursor/rules/. || echo "could not create cursor rules"
+cp -r ~/dotfiles/.cursor/rules/* /workspace/.cursor/rules/. || echo "could not create cursor rules"
 
 sudo apt update
 sudo apt install -y tmux vim
@@ -29,3 +29,9 @@ mkdir -p ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts || echo "could not create known_hosts"
 mkdir -p /home/vscode/.ssh
 ssh-keyscan github.com >> /home/vscode/.ssh/known_hosts || echo "could not create known_hosts for remote user"
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git /home/vscode/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
+vim +PluginInstall +qall
